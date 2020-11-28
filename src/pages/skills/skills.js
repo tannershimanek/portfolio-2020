c7d0369a87125a5a45e47052af6615b3fabb398a
tannershimanek <tannershimanek@gmail.com>
2020-11-27 20:30:15 -0700

    updated skills page

--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -7,6 +7,22 @@ const skillsPage = () => {
             <h1>Skills Page Coming Soon!</h1>
             <br />
             <img src="https://media.giphy.com/media/M9kgjEsLG6LMbYC9dl/giphy.gif"/>
+
+            <ul>
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>CSS3</li>
+                <li>HTML5</li>
+                <li>Java</li>
+                <li>C++</li>
+                <li>Docker</li>
+                <li>Kubernetes</li>
+                <li>Git</li>
+                <li>SQL</li>
+                <li>GraphQL</li>
+            </ul>
         </div>
     );
 };
